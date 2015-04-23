692d263ffd512f0e76a5237bec93f3ca74dfef74
Guyunteng <ak1945@qq.com>
2015-04-23 11:15:48 +0800

    Update ImplicitEuler.m

--- ImplicitEuler.m
+++ ImplicitEuler.m
@@ -1,3 +1,10 @@
+% Implicit-Euler method
+% a,b is the range for independent variable t.
+% u0 is the initial value for u(0)=u0
+% h is the length of iterative step length
+% f(t,u) is the right side of fundamental form ODE: du/dt=f(t,u)
+% the output is discrete points of t,u
+
 function [t,u]=ImplicitEuler(a,b,u0,h)
 t=a:h:b;
 u(1)=u0;
