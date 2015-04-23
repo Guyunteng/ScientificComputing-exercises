a1e3d27b7f0b1009d8ca2f77cf1657c643e50a9c
Guyunteng <ak1945@qq.com>
2015-04-23 11:11:18 +0800

    Update Euler.m

--- Euler.m
+++ Euler.m
@@ -1,4 +1,11 @@
-function [t,u]=Euler(a,b,u0,h)
+% Euler method
+% a,b is the range for independent variable t.
+% u0 is the initial value for u(0)=u0
+% h is the length of iterative step length
+% f(t,u) is the right side of fundamental form ODE: du/dt=f(t,u)
+% the output is discrete points of t,u
+function [t,u]=Euler(a,b,u0,h) 
+
 t=a:h:b;
 u(1)=u0;
 for (n=1:1:length(t)-1)
